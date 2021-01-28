864231824535cf4fc9f6fd5507b42be247662615
Dmitri So <dmitriso@yahoo.com>
2021-01-28 17:53:46 -0500

    Reverting changes made from last commit.

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -5,7 +5,7 @@ const request = indexedDB.open("budget", 1);
 request.onupgradeneeded = function(event) {
    // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
-  db.createObjectStore("transactions", { autoIncrement: true });
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
@@ -23,10 +23,10 @@ request.onerror = function(event) {
 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
-  const transaction = db.transaction(["transactions"], "readwrite");
+  const transaction = db.transaction(["pending"], "readwrite");
 
   // access your pending object store
-  const store = transaction.objectStore("transactions");
+  const store = transaction.objectStore("pending");
 
   // add record to your store with add method.
   store.add(record);
@@ -34,9 +34,9 @@ function saveRecord(record) {
 
 function checkDatabase() {
   // open a transaction on your pending db
-  const transaction = db.transaction(["transactions"], "readwrite");
+  const transaction = db.transaction(["pending"], "readwrite");
   // access your pending object store
-  const store = transaction.objectStore("transactions");
+  const store = transaction.objectStore("pending");
   // get all records from store and set to a variable
   const getAll = store.getAll();
 
@@ -53,10 +53,10 @@ function checkDatabase() {
       .then(response => response.json())
       .then(() => {
         // if successful, open a transaction on your pending db
-        const transaction = db.transaction(["transactions"], "readwrite");
+        const transaction = db.transaction(["pending"], "readwrite");
 
         // access your pending object store
-        const store = transaction.objectStore("transactions");
+        const store = transaction.objectStore("pending");
 
         // clear all items in your store
         store.clear();
